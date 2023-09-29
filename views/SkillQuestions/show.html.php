39232ea3530a9132987aba96ea9f73ba87d27bf0
Jeff Plumb <quiet.year9434@plumbmail.com>
2023-09-29 15:45:22 +1000

    Small change to appearance of question

--- views/SkillQuestions/show.html.php
+++ views/SkillQuestions/show.html.php
@@ -5,17 +5,15 @@ include __DIR__.'/../layout/navbar.html.php';
     <div class="container">
       <div class="row">
         <div class="col-12 worksheetQuestionWrapper">
-          <div class="worksheetQuestion">
-            <p><?= $this->esc($question['question']) ?></p>
-            <?php if ($question['question_image']) { ?>
-              <img src="/uploads/skill-questions/<?= $this->esc($question['question_image']); ?>" alt="<?= $this->esc($question['question_image']); ?>" class="questionImage"/>
+          <p><?= $this->esc($question['question']) ?></p>
+          <?php if ($question['question_image']) { ?>
+            <img src="/uploads/skill-questions/<?= $this->esc($question['question_image']); ?>" alt="<?= $this->esc($question['question_image']); ?>" class="questionImage"/>
+          <?php } ?>
+          <?php if ($question['skill_question_type_id'] == 1) { ?>
+            <?php foreach ($options as $option) { ?>
+              <p><?= $this->esc($option['option_letter'].' : '.$option['option_text']); ?></p>
             <?php } ?>
-            <?php if ($question['skill_question_type_id'] == 1) { ?>
-              <?php foreach ($options as $option) { ?>
-                <p><?= $this->esc($option['option_letter'].' : '.$option['option_text']); ?></p>
-              <?php } ?>
-            <?php } ?>
-          </div><!-- worksheetQuestion -->
+          <?php } ?>
         </div><!-- col-4 -->
       </div><!-- row -->
     </div><!-- container -->
